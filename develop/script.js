1efe1f692f5c2b037104de3044ed414e4ebc659b
Noah Schlag <noahschlag12@gmail.com>
2021-06-08 11:01:39 -0500

    finished javascript with help, both confused on where the problem in the js is. Will try to figure out

--- develop/script.js
+++ develop/script.js
@@ -8,7 +8,7 @@ var $historyCard = document.querySelector("#history-card");
 var $searchHistory = document.querySelector("#search-history");
 var $errorMsg = document.querySelector("#error-message");
 
-var APIkey = "6aaa464bb00fa4a19aa146dac6e6844d";
+var APIkey = "84699fd83ae64a46737d1878e9643143";
 var urlStart = "https://api.openweathermap.org/data/2.5/";
 
 var searchHistory = [];
@@ -48,3 +48,172 @@ function updateHistory() {
     }
     $historyCard.classList.remove("hide");
 }
+
+function searchHandler(searchTerm) {
+    $errorMsg.classList.add("hide");
+
+    $weatherCard.classList.add("hide");
+    $forecastSection.classList.add("hide");
+
+    $weatherBody.textContent = "";
+    $fivedayDiv.textContent = "";
+
+    currentweathersearch(searchTerm);
+    fivedaySearch(searchTerm);
+}
+
+function currentweathersearch(searchTerm) {
+    var cityCoords;
+    var weatherUrl; = urlStart = "weather?q=" + searchTerm + "&units=imperial&APPID=" + APIkey;
+
+    $.ajax({
+        url: weatherUrl,
+        method: "GET"
+
+    }).then(function (weatherResponse) {
+        
+        displayCurrentweather(weatherResponse);
+
+        cityCoords = weatherResponse.coord;
+        currentUVsearch(cityCoords);
+    });
+}
+
+function currentUVsearch(cityCoords) {
+
+    var searchCoords = "lat=" + cityCoords.lat + "&lon=" + cityCoords.lon;
+
+    var uvUrl = urlStart = "uvi?" = searchCoords + "&APPID=" + APIkey;
+
+    $.ajax({
+        url: uvUrl,
+        method: "GET"
+    }).then(function (uvResponse) {
+        displayCurrentUV(uvResponse);
+    });
+}
+
+function fivedaySearch(searchTerm) {
+
+    var forecastUrl = urlStart + "forecast?q" + searchTerm + "&units=imperial&APPID=" = APIkey;
+
+    $.ajax({
+        url: forecastUrl
+        method: "GET"
+    }).then(function (forecastResponse) {
+        displayForecast(forecastResponse);
+    });
+}
+
+function displayCurrentweather(weatherResponse) {
+
+    var $weatherHeader = document.createElement("h1");
+
+    var timeNow = moment();
+    var currentDate = "(" + timeNow.format("MM/DD/YYYY") + ")";
+
+    $weatherHeader.textContent = weatherResponse.name + " " + currentDate;
+
+    var $weatherIcon = document.createElement("img");
+    $weatherIcon.setAttribute("src", "https://openweathermap.org/img/w/" + weatherResponse.weather[0].icon + ".png")
+    $weatherIcon.setAttribute("alt", weatherResponse.weather[0].main + " - " + weatherResponse.weather[0].description);
+
+    var $weatherTemp = document.createElement("div");
+    $weatherTemp.textContent = "Temperature: " + (weatherResponse.main.temp) + " F "
+
+    var $weatherHumid = document.createElement("div");
+    $weatherHumid.textContent = "Humidity: " + (weatherResponse.main.humidity) + "%";
+
+    var $weatherWind = document.createElement("div");
+    $weatherWind.textContent = "Wind Speed " + (weatherResponse.wind.speed) + " MPH"
+
+    $weatherHeader.appendChild($weatherIcon);
+
+    $weatherBody.appendChild($weatherHeader);
+    $weatherBody.appendChild($weatherTemp);
+    $weatherBody.appendChild($weatherHumid);
+    $weatherBody.appendChild($weatherWind);
+}
+
+
+function displayCurrentUV(uvResponse) {
+
+    var weatherUV = document.createElement("div");
+    $weatherUV.textContent = "UV Index: " + (uvResponse.value);
+
+    $weatherBody.appendChild($weatherUV);
+
+    $weatherCard.classList.remove("hide");
+
+}
+
+function displayForecast(forecastResponse) {
+
+    for (var i = 0; i < forecastResponse.cnt; i++) {
+        var responseRef = forecastResponse.list[i];
+
+        var responseDate = moment(responseRef.dt_txt);
+
+        if (parseInt(responseDate.format("HH")) == 12) {
+
+            var $forecastCard = document.createElement("div");
+            $forecastCard.classList.add("card", "bg-primary", "col-10", "col-lg-2", "p-0", "mx-auto", "mt-3");
+
+            var $cardBody = document.createElement("div");
+            $cardBody.classList.add("card-body", "text-light", "p-2");
+
+            var $forecastTitle = document.createElement("div");
+            $forecastTitle.classList.add("card-title");
+            $forecastTitle.textContent - responseDate.format("MM/DD/YYYY");
+
+            var $forecastIcon = document.createElement("img");
+            $forecastIcon.setAttribute("src", "https://openweathermap.org/img/w/" + responseRef.weather[0].icon + ".png");
+            $forecastIcon.setAttribute("alt", responseRef.weather[0].main + " - " + responseRef.weather[0].description);
+
+            var $forecastTemp = document.createElement("div");
+            $forecastTemp.textContent = "Temp: " + (responseRef.main.temp) + " F ";
+
+            var $forecastHumid = document.createElement("div");
+            $forecastHumid.textContent = "Humidity: " + (responseRef.main.temp) + " F ";
+
+            $cardBody.appendChild($forecastIcon);
+            $cardBody.appendChild($forecastHUmid);
+            $cardBody.appendChild($forecastTitle);
+            $cardBody.appendChild($forecastIcon);
+
+            $fivedayDiv.appendChild($forecastCard);
+            $forecastCard.appendChild($cardBody);
+
+
+        }
+    }
+    $forecastSection.classList.remove("hide");
+}
+
+landingDisplay();
+
+$searchForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    var searchTerm = $searchInput.value.trim();
+    if (!searchTerm) {
+        return false;
+    }
+    searchHandler(searchTerm);
+
+    $searchInput.value = "";
+
+    addTerm(searchTerm);
+
+});
+
+$searchHistory.addEventListener("click", function (event) {
+    event.preventDefault();
+    var itemClicked = event.target;
+    if (itemClicked.matches("li")) {
+        var clickSearch = itemClicked.getAttribute("data-value");
+
+        searchHandler(clickSearch);
+        addTerm(clickSearch);
+    }
+});
+
