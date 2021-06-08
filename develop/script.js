63260833c436ed7d41ba613aaf6233035f465c07
Noah Schlag <noahschlag12@gmail.com>
2021-06-08 01:01:10 -0500

    changed js around, looked up help. Found a good tutorial and saw I was going about it completely wrong

--- develop/script.js
+++ develop/script.js
@@ -1,22 +1,50 @@
-const form = document.querySelector(".top-banner form");
+var $searchForm = document.querySelector("#city-search");
+var $searchInput = document.querySelector("#search-input");
+var $weatherCard = document.querySelector("#current-weather");
+var $weatherBody = document.querySelector("#weather-body");
+var $forecastSection = document.querySelector("#forecast-section");
+var $fivedayDiv = document.querySelector("#five-day");
+var $historyCard = document.querySelector("#history-card");
+var $searchHistory = document.querySelector("#search-history");
+var $errorMsg = document.querySelector("#error-message");
 
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    const inputVal = input.value;
-});
+var APIkey = "6aaa464bb00fa4a19aa146dac6e6844d";
+var urlStart = "https://api.openweathermap.org/data/2.5/";
 
-const apiKey = "84699fd83ae64a46737d1878e9643143";
-const inputVal = input.value;
+var searchHistory = [];
 
-...
+function landingDisplay() {
+    if (localStorage.getItem("searchHistory")) {
+        updateHistory();
+        searchHandler(searchHistory[searchHistory.length - 1]);
+    }
+}
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+function addTerm(searchTerm) {
+    if (localStorage.getItem("searchHistory")) {
+        searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    }
+    searchHistory.push(searchTerm);
 
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-    
-    })
-    .catch(() => {
-        msg.textContent = "Search anywhere in the world for the weather you need to know";
-    });
+    if (searchHistory.length > 5) {
+        searchHistory.shift();
+    }
+
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+
+    updateHistory();
+}
+
+function updateHistory() {
+    $searchHistory.textContent = "";
+    searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
+    for (var j = searchHistory.length - 1; j >= 0; j--) {
+        var $pastSearch = document.createElement("li");
+        $pastSearch.textContent = searchHistory[j];
+        $pastSearch.classList.add("list-group-item");
+        $pastSearch.setAttribute("data-value", searchHistory[j]);
+
+        $searchHistory.appendChild($pastSearch);
+    }
+    $historyCard.classList.remove("hide");
+}
